3e2c9d7393b848ab4261800b033f0717ec077a41
Iavor Orlyov <i.orlyov@drafkings.com>
2022-06-28 21:25:08 +0300

    SubmitCodeResult added

---
+++ Magnus.Futbot.Common/Models/Results/SubmitCodeResult.cs
@@ -0,0 +1,19 @@
+﻿using Magnus.Futbot.Common.Models.Selenium.Profiles;
+using MongoDB.Bson;
+
+namespace Magnus.Futbot.Common.Models.Results
+{
+    public class SubmitCodeResult
+    {
+        public SubmitCodeResult(SubmitCodeDTO submitCodeDTO, ConfirmationCodeStatusType confirmationCodeStatusType)
+        {
+            Email = submitCodeDTO.Email;
+            UserId = submitCodeDTO.UserId;
+            ConfirmationCodeStatus = confirmationCodeStatusType;
+        }
+
+        public string Email { get; }
+        public ConfirmationCodeStatusType ConfirmationCodeStatus { get; }
+        public ObjectId UserId { get; }
+    }
+}
