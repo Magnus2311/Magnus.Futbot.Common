47685fdb65da9d04c3691bed6a9cdb1e19dc8d07
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-07-05 21:04:26 +0300

    Added serializer and deserializer for kafka messages

---
+++ Magnus.Futbot.Common/Models/Kafka/Serialization/Deserializer.cs
@@ -0,0 +1,12 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Magnus.Futbot.Common.Models.Kafka.Serialization
+{
+    public class Deserializer<T> : IDeserializer<T>
+    {
+        public T Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+            => JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(data))!;
+    }
+}
---
+++ Magnus.Futbot.Common/Models/Kafka/Serialization/Serializer.cs
@@ -0,0 +1,12 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Magnus.Futbot.Common.Models.Kafka.Serialization
+{
+    public class Serializer<T> : ISerializer<T>
+    {
+        public byte[] Serialize(T data, SerializationContext context)
+            => Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data));
+    }
+}
