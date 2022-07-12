e13c7446449a9f6a1bbb7b6d5ca4fed3306c7256
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-07-12 16:37:00 +0300

    Added abstract GroupId

--- Magnus.Futbot.Common/Models/Kafka/BaseConsumer.cs
+++ Magnus.Futbot.Common/Models/Kafka/BaseConsumer.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace Magnus.Futbot.Common.Models.Kafka
 {
+
     public abstract class BaseConsumer<TKey, TValue>
     {
         public BaseConsumer(IConfiguration configuration)
@@ -13,7 +14,7 @@ namespace Magnus.Futbot.Common.Models.Kafka
             var config = new ConsumerConfig
             {
                 BootstrapServers = Configuration["URLS:KAFKA:SERVER"],
-                GroupId = GetType().Name,
+                GroupId = GroupId,
                 AutoOffsetReset = AutoOffsetReset.Earliest
             };
 
@@ -23,12 +24,12 @@ namespace Magnus.Futbot.Common.Models.Kafka
             Consumer.Subscribe(Topic);
         }
 
-
         public IConsumer<TKey, TValue> Consumer { get; }
         public bool Cancelled { get; set; }
         public CancellationToken CancellationToken { get; set; }
 
         public abstract string Topic { get; }
+        public abstract string GroupId { get; }
 
         protected IConfiguration Configuration { get; }
 
